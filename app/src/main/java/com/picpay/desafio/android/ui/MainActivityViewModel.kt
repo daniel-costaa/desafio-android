e0c961c68d50b54f8a88d369adc492f01f0848f6
Daniel Costa <daniel.costa@syngenta.com>
2022-09-26 17:19:12 -0300

    Created view model

---
+++ app/src/main/java/com/picpay/desafio/android/ui/MainActivityViewModel.kt
@@ -0,0 +1,8 @@
+package com.picpay.desafio.android.ui
+
+import androidx.lifecycle.ViewModel
+import com.picpay.desafio.android.data.PicPayRepository
+
+class MainActivityViewModel(private val picPayRepository: PicPayRepository) : ViewModel() {
+
+}
