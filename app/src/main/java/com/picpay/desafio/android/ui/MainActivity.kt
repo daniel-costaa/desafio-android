71071dc4ce0f62d1334769957131501880773815
Daniel Costa <daniel.costa@syngenta.com>
2022-09-26 19:32:15 -0300

    Improved main activity

--- app/src/main/java/com/picpay/desafio/android/ui/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/MainActivity.kt
@@ -1,33 +1,50 @@
 package com.picpay.desafio.android.ui
 
+import android.os.Bundle
 import android.view.View
-import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.picpay.desafio.android.R
 import com.picpay.desafio.android.data.Resource
 import com.picpay.desafio.android.data.User
+import com.picpay.desafio.android.databinding.ActivityMainBinding
 import com.picpay.desafio.android.ui.adapters.UserListAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var progressBar: ProgressBar
-    private lateinit var adapter: UserListAdapter
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var userListAdapter: UserListAdapter
     private val viewModel by viewModel<MainActivityViewModel>()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
 
     override fun onStart() {
         super.onStart()
+        setupRecyclerView()
         setupObservers()
+        viewModel.getUsers()
+    }
+
+    private fun setupRecyclerView() {
+        with(binding.recyclerView) {
+            userListAdapter = UserListAdapter()
+            adapter = userListAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
     }
 
     private fun setupObservers() {
         viewModel.users.observe(this) { resource ->
             when (resource.status) {
-                Resource.Status.LOADING -> progressBar.visibility = View.VISIBLE
+                Resource.Status.LOADING -> binding.userListProgressBar.visibility = View.VISIBLE
                 Resource.Status.ERROR -> onError()
                 Resource.Status.SUCCESS -> onSuccess(resource.data ?: emptyList())
             }
@@ -35,31 +52,17 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     }
 
     private fun onSuccess(users: List<User>) {
-        progressBar.visibility = View.GONE
+        binding.userListProgressBar.visibility = View.GONE
 
-        adapter.users = users
+        userListAdapter.users = users
     }
 
     private fun onError() {
         val message = getString(R.string.error)
 
-        progressBar.visibility = View.GONE
-        recyclerView.visibility = View.GONE
+        binding.userListProgressBar.visibility = View.GONE
+        binding.recyclerView.visibility = View.GONE
 
-        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT)
-            .show()
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        recyclerView = findViewById(R.id.recyclerView)
-        progressBar = findViewById(R.id.user_list_progress_bar)
-
-        adapter = UserListAdapter()
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        viewModel.getUsers()
+        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
     }
 }
