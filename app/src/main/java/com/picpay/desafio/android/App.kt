7feaef0e2ea81ebbd94802b0ab7e5976ff222b51
Daniel Costa <daniel.costa@syngenta.com>
2022-09-26 17:10:38 -0300

    Created network module

--- app/src/main/java/com/picpay/desafio/android/App.kt
+++ app/src/main/java/com/picpay/desafio/android/App.kt
@@ -1,13 +1,18 @@
 package com.picpay.desafio.android
 
 import android.app.Application
+import com.picpay.desafio.android.di.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-
+            androidLogger()
+            androidContext(this@App)
+            modules(networkModule)
         }
     }
 }
