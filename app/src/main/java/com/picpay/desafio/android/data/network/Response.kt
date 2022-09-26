cf4951d13f95755188354fd1146d33d77cf771a0
Daniel Costa <daniel.costa@syngenta.com>
2022-09-26 17:48:41 -0300

    Removed response wrapper

--- app/src/main/java/com/picpay/desafio/android/data/network/Response.kt
+++
@@ -1,6 +0,0 @@
-package com.picpay.desafio.android.data.network
-
-sealed interface Response<out T> {
-    data class Success<T>(val data: T) : Response<T>
-    data class Error<T>(val code: Int) : Response<T>
-}
--- app/src/main/java/com/picpay/desafio/android/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/MainActivityViewModel.kt
@@ -6,27 +6,26 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.picpay.desafio.android.data.PicPayRepository
 import com.picpay.desafio.android.data.User
-import com.picpay.desafio.android.data.network.Response
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
-import retrofit2.Response as RetrofitResponse
+import retrofit2.Response
 
 class MainActivityViewModel(private val picPayRepository: PicPayRepository) : ViewModel() {
-    private val _users = MutableLiveData<Response<List<User>>>()
-    val users: LiveData<Response<List<User>>> = _users
+    private val _users = MutableLiveData<List<User>>()
+    val users: LiveData<List<User>> = _users
 
     fun getUsers() {
         viewModelScope.launch {
             picPayRepository.getUsers().enqueue(object : Callback<List<User>> {
-                override fun onResponse(call: Call<List<User>>, response: RetrofitResponse<List<User>>) {
+                override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
                     response.body()?.let {
-                        _users.postValue(Response.Success(it))
+                        _users.postValue(it)
                     }
                 }
 
                 override fun onFailure(call: Call<List<User>>, t: Throwable) {
-                    _users.postValue(Response.Error(0))
+                    _users.postValue(emptyList())
                 }
             })
         }
