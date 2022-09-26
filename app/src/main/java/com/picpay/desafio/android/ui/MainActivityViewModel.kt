046ad7cc98040b76cfec5d06f2e42a4cc7564d15
Daniel Costa <daniel.costa@syngenta.com>
2022-09-26 17:28:49 -0300

    Created function in view model to fetch users

--- app/src/main/java/com/picpay/desafio/android/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/MainActivityViewModel.kt
@@ -1,8 +1,33 @@
 package com.picpay.desafio.android.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.picpay.desafio.android.data.PicPayRepository
+import com.picpay.desafio.android.data.User
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivityViewModel(private val picPayRepository: PicPayRepository) : ViewModel() {
+    private val _users = MutableLiveData<List<User>>()
+    val users: LiveData<List<User>> = _users
 
+    fun getUsers() {
+        viewModelScope.launch {
+            picPayRepository.getUsers().enqueue(object : Callback<List<User>> {
+                override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                    response.body()?.let {
+                        _users.postValue(it)
+                    }
+                }
+
+                override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                    _users.postValue(emptyList())
+                }
+            })
+        }
+    }
 }
