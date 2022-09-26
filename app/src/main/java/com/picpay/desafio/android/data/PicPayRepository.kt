7adecc34fbd7907e43341c08663917b50a352cb4
Daniel Costa <daniel.costa@syngenta.com>
2022-09-26 18:48:44 -0300

    Improved repository

--- app/src/main/java/com/picpay/desafio/android/data/PicPayRepository.kt
+++ app/src/main/java/com/picpay/desafio/android/data/PicPayRepository.kt
@@ -3,5 +3,12 @@ package com.picpay.desafio.android.data
 import com.picpay.desafio.android.data.network.PicPayService
 
 class PicPayRepository(private val picPayService: PicPayService) {
-    suspend fun getUsers() = picPayService.getUsers()
+    suspend fun getUsers(): Resource<List<User>> {
+        return try {
+            val users = picPayService.getUsers()
+            Resource.success(users)
+        } catch (e: Exception) {
+            Resource.error(e.message.toString(), null)
+        }
+    }
 }
