c0128b2d13ab3116d39654eb2fd3c999ce4740ac
Daniel Costa <daniel.costa@syngenta.com>
2022-09-26 17:42:56 -0300

    Created logic in view model to fetch users

--- app/src/main/java/com/picpay/desafio/android/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/MainActivityViewModel.kt
@@ -6,26 +6,27 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.picpay.desafio.android.data.PicPayRepository
 import com.picpay.desafio.android.data.User
+import com.picpay.desafio.android.data.network.Response
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
-import retrofit2.Response
+import retrofit2.Response as RetrofitResponse
 
 class MainActivityViewModel(private val picPayRepository: PicPayRepository) : ViewModel() {
-    private val _users = MutableLiveData<List<User>>()
-    val users: LiveData<List<User>> = _users
+    private val _users = MutableLiveData<Response<List<User>>>()
+    val users: LiveData<Response<List<User>>> = _users
 
     fun getUsers() {
         viewModelScope.launch {
             picPayRepository.getUsers().enqueue(object : Callback<List<User>> {
-                override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                override fun onResponse(call: Call<List<User>>, response: RetrofitResponse<List<User>>) {
                     response.body()?.let {
-                        _users.postValue(it)
+                        _users.postValue(Response.Success(it))
                     }
                 }
 
                 override fun onFailure(call: Call<List<User>>, t: Throwable) {
-                    _users.postValue(emptyList())
+                    _users.postValue(Response.Error(0))
                 }
             })
         }
