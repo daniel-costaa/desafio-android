b2d8ae0b3e874774f655ec73a2669f3870838a79
Daniel Costa <daniel.costa@syngenta.com>
2022-09-26 18:48:53 -0300

    Improved view model

--- app/src/main/java/com/picpay/desafio/android/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/MainActivityViewModel.kt
@@ -5,29 +5,19 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.picpay.desafio.android.data.PicPayRepository
+import com.picpay.desafio.android.data.Resource
 import com.picpay.desafio.android.data.User
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivityViewModel(private val picPayRepository: PicPayRepository) : ViewModel() {
-    private val _users = MutableLiveData<List<User>>()
-    val users: LiveData<List<User>> = _users
+    private val _users = MutableLiveData<Resource<List<User>>>()
+    val users: LiveData<Resource<List<User>>> = _users
 
     fun getUsers() {
         viewModelScope.launch {
-            picPayRepository.getUsers().enqueue(object : Callback<List<User>> {
-                override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
-                    response.body()?.let {
-                        _users.postValue(it)
-                    }
-                }
-
-                override fun onFailure(call: Call<List<User>>, t: Throwable) {
-                    _users.postValue(emptyList())
-                }
-            })
+            _users.postValue(Resource.loading(null))
+            val resource = picPayRepository.getUsers()
+            _users.postValue(resource)
         }
     }
 }
