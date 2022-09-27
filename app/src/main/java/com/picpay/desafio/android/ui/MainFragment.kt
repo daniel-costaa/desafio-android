e69f8145111c2fc2ffed861adeed2374d0969503
Daniel Costa <daniel.costa@syngenta.com>
2022-09-27 18:46:09 -0300

    Implemented view binding in fragment

--- app/src/main/java/com/picpay/desafio/android/ui/MainFragment.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/MainFragment.kt
@@ -1,7 +1,9 @@
 package com.picpay.desafio.android.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,9 +15,26 @@ import com.picpay.desafio.android.ui.adapters.UserListAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment() {
-    private lateinit var binding: FragmentMainBinding
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
     private lateinit var userListAdapter: UserListAdapter
     private val viewModel by viewModel<MainViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
