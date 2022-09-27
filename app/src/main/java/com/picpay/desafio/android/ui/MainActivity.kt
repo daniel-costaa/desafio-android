a73ce5f177a4e5f33ccce0e0c3c9ba2442357f2a
Daniel Costa <daniel.costa@syngenta.com>
2022-09-27 18:44:01 -0300

    Migrated activity to fragment

--- app/src/main/java/com/picpay/desafio/android/ui/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/MainActivity.kt
@@ -1,22 +1,12 @@
 package com.picpay.desafio.android.ui
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.picpay.desafio.android.R
-import com.picpay.desafio.android.data.model.Resource
-import com.picpay.desafio.android.data.model.UserDomain
 import com.picpay.desafio.android.databinding.ActivityMainBinding
-import com.picpay.desafio.android.ui.adapters.UserListAdapter
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
     private lateinit var binding: ActivityMainBinding
-    private lateinit var userListAdapter: UserListAdapter
-    private val viewModel by viewModel<MainActivityViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,44 +14,5 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         val view = binding.root
         setContentView(view)
     }
-
-    override fun onStart() {
-        super.onStart()
-        setupRecyclerView()
-        setupObservers()
-        viewModel.getUsers()
-    }
-
-    private fun setupRecyclerView() {
-        with(binding.recyclerView) {
-            userListAdapter = UserListAdapter()
-            adapter = userListAdapter
-            layoutManager = LinearLayoutManager(this@MainActivity)
-        }
-    }
-
-    private fun setupObservers() {
-        viewModel.users.observe(this) { resource ->
-            when (resource.status) {
-                Resource.Status.LOADING -> binding.userListProgressBar.visibility = View.VISIBLE
-                Resource.Status.ERROR -> onError()
-                Resource.Status.SUCCESS -> onSuccess(resource.data ?: emptyList())
-            }
-        }
-    }
-
-    private fun onSuccess(users: List<UserDomain>) {
-        binding.userListProgressBar.visibility = View.GONE
-
-        userListAdapter.users = users
-    }
-
-    private fun onError() {
-        val message = getString(R.string.error)
-
-        binding.userListProgressBar.visibility = View.GONE
-        binding.recyclerView.visibility = View.GONE
-
-        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
-    }
 }
+
--- app/src/main/java/com/picpay/desafio/android/ui/MainFragment.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/MainFragment.kt
@@ -1,60 +1,59 @@
 package com.picpay.desafio.android.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.picpay.desafio.android.R
+import com.picpay.desafio.android.data.model.Resource
+import com.picpay.desafio.android.data.model.UserDomain
+import com.picpay.desafio.android.databinding.FragmentMainBinding
+import com.picpay.desafio.android.ui.adapters.UserListAdapter
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentMainBinding
+    private lateinit var userListAdapter: UserListAdapter
+    private val viewModel by viewModel<MainActivityViewModel>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        setupObservers()
+        viewModel.getUsers()
+    }
+
+    private fun setupRecyclerView() {
+        with(binding.recyclerView) {
+            userListAdapter = UserListAdapter()
+            adapter = userListAdapter
+            layoutManager = LinearLayoutManager(requireContext())
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    private fun setupObservers() {
+        viewModel.users.observe(viewLifecycleOwner) { resource ->
+            when (resource.status) {
+                Resource.Status.LOADING -> binding.userListProgressBar.visibility = View.VISIBLE
+                Resource.Status.ERROR -> onError()
+                Resource.Status.SUCCESS -> onSuccess(resource.data ?: emptyList())
+            }
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun onSuccess(users: List<UserDomain>) {
+        binding.userListProgressBar.visibility = View.GONE
+
+        userListAdapter.users = users
+    }
+
+    private fun onError() {
+        val message = getString(R.string.error)
+
+        binding.userListProgressBar.visibility = View.GONE
+        binding.recyclerView.visibility = View.GONE
+
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 }
