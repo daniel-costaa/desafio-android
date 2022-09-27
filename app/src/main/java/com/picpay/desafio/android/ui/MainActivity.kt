57c1503b42226f12768770a6583ecf594583cf55
Daniel Costa <daniel.costa@syngenta.com>
2022-09-27 18:47:35 -0300

    Fixed activity

--- app/src/main/java/com/picpay/desafio/android/ui/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/MainActivity.kt
@@ -2,10 +2,9 @@ package com.picpay.desafio.android.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.picpay.desafio.android.R
 import com.picpay.desafio.android.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
