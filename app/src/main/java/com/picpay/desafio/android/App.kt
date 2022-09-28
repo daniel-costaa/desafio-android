5ad9a110c529a97f0a2e3b76c8c070ff16678449
Daniel Costa <daniel.costa@syngenta.com>
2022-09-28 19:06:35 -0300

    Added module to koin

--- app/src/main/java/com/picpay/desafio/android/App.kt
+++ app/src/main/java/com/picpay/desafio/android/App.kt
@@ -1,6 +1,7 @@
 package com.picpay.desafio.android
 
 import android.app.Application
+import com.picpay.desafio.android.di.databaseModule
 import com.picpay.desafio.android.di.networkModule
 import com.picpay.desafio.android.di.repositoryModule
 import com.picpay.desafio.android.di.viewModelModule
@@ -14,7 +15,14 @@ class App : Application() {
         startKoin {
             androidLogger()
             androidContext(this@App)
-            modules(listOf(networkModule, repositoryModule, viewModelModule))
+            modules(
+                listOf(
+                    networkModule,
+                    repositoryModule,
+                    viewModelModule,
+                    databaseModule
+                )
+            )
         }
     }
 }
