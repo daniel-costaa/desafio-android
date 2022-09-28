b41608a4f4cf7de6aca9250466e5615bb75a2379
Daniel Costa <daniel.costa@syngenta.com>
2022-09-28 18:57:36 -0300

    Updated DAO

--- app/src/main/java/com/picpay/desafio/android/data/dao/UserDao.kt
+++ app/src/main/java/com/picpay/desafio/android/data/dao/UserDao.kt
@@ -1,6 +1,7 @@
 package com.picpay.desafio.android.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import com.picpay.desafio.android.data.model.UserEntity
 
@@ -8,4 +9,7 @@ import com.picpay.desafio.android.data.model.UserEntity
 interface UserDao {
     @Query("SELECT * FROM user")
     suspend fun getAllUsers(): List<UserEntity>
+
+    @Insert
+    fun saveUsers(users: List<UserEntity>)
 }
