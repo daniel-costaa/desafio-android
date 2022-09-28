2dfe902c8857eaafcf75bb46cbfd2af4fc4a14b3
Daniel Costa <daniel.costa@syngenta.com>
2022-09-28 20:32:06 -0300

    Created tests for repository

--- app/src/test/java/com/picpay/desafio/android/data/PicPayRepositoryTest.kt
+++ app/src/test/java/com/picpay/desafio/android/data/PicPayRepositoryTest.kt
@@ -4,6 +4,7 @@ import com.picpay.desafio.android.data.dao.UserDao
 import com.picpay.desafio.android.data.datasources.PicPayService
 import com.picpay.desafio.android.data.model.Resource
 import com.picpay.desafio.android.data.model.UserDto
+import com.picpay.desafio.android.data.model.UserEntity
 import com.picpay.desafio.android.data.repository.PicPayRepository
 import io.mockk.*
 import junit.framework.Assert.assertEquals
@@ -29,8 +30,42 @@ class PicPayRepositoryTest {
         val result = runBlocking {
             picPayRepository.getUsers()
         }
+
         // Assert
         assertEquals(Resource.Status.SUCCESS, result.status)
         coVerify { picPayService.getUsers() }
+    }
+
+    @Test
+    fun givenHasCacheThenShouldNotMakeApiCall() {
+        // Mock
+        val entityUser = listOf(UserEntity(1, "", "", ""))
+
+        coEvery { userDao.getAllUsers() } returns entityUser
+        coJustRun { userDao.saveUsers(any()) }
+
+        // Run
+        val result = runBlocking {
+            picPayRepository.getUsers()
+        }
+
+        // Assert
+        assertEquals(Resource.Status.SUCCESS, result.status)
+        coVerify(exactly = 0) { picPayService.getUsers() }
+    }
+
+    @Test
+    fun givenThereIsNetworkProblemThenShouldThrowException() {
+        // Mock
+        coEvery { userDao.getAllUsers() } returns emptyList()
+        coEvery { picPayService.getUsers() } throws Exception("network exception")
+
+        // Run
+        val result = runBlocking {
+            picPayRepository.getUsers()
+        }
+
+        // Assert
+        assertEquals(Resource.Status.ERROR, result.status)
     }
 }
